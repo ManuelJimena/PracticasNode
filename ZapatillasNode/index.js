9e2d945bd3e561dc5feaaf57557c3b8e1e902b0f
Manuel Jimena García <Manuel.jimena29@gmail.com>
2023-11-01 16:16:04 +0100

    refact models

--- ZapatillasNode/index.js
+++ ZapatillasNode/index.js
@@ -3,20 +3,48 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const connect = require("./utils/db")
+const connect = require("./utils/db");
+
+const Zapatilla = require("./models/zapatilla.model");
+
 
 const server = express();
 
+server.use(express.json());
+
+server.use(express.urlencoded({ extended: false }));
 connect();
 
-const  router = express.Router();
+
+const router = express.Router();
 
 //Routes
+router.post("/create", async (req, res) => {
+  try {
+    console.log(newZapatilla);
+    await newZapatilla.save();
+    return res.status(201).json(newZapatilla);
+  } catch (error) {
+    return res.status(400).json("Error creating zapatilla");
+  }
+});
+
+router.get("/zapatillas", async (req, res) => {
+  try {
+    const zapatillas = await Zapatilla.find();
+    return res.status(200).json(zapatillas);
+  } catch (error) {
+    return res.status(404).json("Zapatillas not found");
+  }
+});
+
 
 server.use("/", router);
 
+
 const PORT = process.env.PORT;
 
+
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
--- ZapatillasNode/models/zapatilla.model.js
+++ ZapatillasNode/models/zapatilla.model.js
@@ -1,16 +1,17 @@
 const mongoose = require("mongoose");
 
-const ZapatillaSchema = new mongoose.Schema({
+const ZapatillaSchema = new mongoose.Schema(
+  {
     model: { type: String, required: true },
     brand: { type: String, enum: ["Nike", "Adidas"] },
     image: { type: String, required: true },
     size: { type: Number, required: true },
     outofstock: { type: Boolean, default: false },
     madein: { type: String, required: false },
-},
-{
+  },
+  {
     timestamps: true,
-}
+  }
 );
 
 const Zapatilla = mongoose.model("zapatilla", ZapatillaSchema);
