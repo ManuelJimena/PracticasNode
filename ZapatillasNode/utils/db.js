6fe7c838ee172273cc223fb5e9f7d65c72a9c92c
Admin <Manuel.jimena29@gmail.com>
2023-11-03 08:49:53 +0100

    refact mogo_uri

--- ZapatillasNode/utils/db.js
+++ ZapatillasNode/utils/db.js
@@ -9,10 +9,7 @@ const MONGO_URI = process.env.MONGO_URI;
 
 const connect = async () => {
   try {
-    const db = await mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const db = await mongoose.connect(MONGO_URI);
     const { name, host } = db.connection;
     console.log(`Connected to DB: ${name}, in host: ${host}`);
   } catch (error) {
